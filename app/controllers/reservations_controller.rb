c830fb6a9fd87783e429fc33cd7233867896a581
Busiwa24 <busiwaliuma@gmail.com>
2022-04-04 15:50:27 +0200

    Add create and remove reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -11,4 +11,17 @@ class ReservationsController < ApplicationController
       render json: cruise, adapter: :json_api, status: :ok
     end
   
-   
+    api :POST, '/users/:user_id/reservations', 'Create a new reservation for meeting'
+    def create
+      reservation = Reservation.create(user_id: params[:user_id], meeting_id: params[:meeting_id])
+      render json: reservation, adapter: :json_api, status: :ok if reservation.save!
+    end
+  
+    api :DELETE, '/users/:user_id/reservations/:id', 'Delete a reservation for a meeting'
+    def destroy
+      reservation = Reservation.find(params[:id])
+      reservation.destroy
+      render json: { message: 'Reservation has been deleted succesfully!', reservation: reservation.meeting.title },
+             adapter: :json_api, status: :ok
+    end
+  end
