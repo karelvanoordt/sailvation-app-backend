b104ee2f0f34de959763ddd5f013e031877e217c
Karel van Oordt <vanoordtkarel@gmail.com>
2022-04-04 08:17:39 -0500

    Add routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users do
+    resources :cruises, only: %i[index show create destroy]
+    resources :reservations, only: %i[index show create destroy]
+  end
 end
