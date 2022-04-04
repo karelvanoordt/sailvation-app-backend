84aa0bdf32e758a3ecaf4a33ac48122c0eb956b7
Busiwa24 <busiwaliuma@gmail.com>
2022-04-04 21:14:30 +0200

    Restructure code

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,27 +1,35 @@
 class ReservationsController < ApplicationController
-    api :GET, '/users/:user_id/reservations', "This is index view for user's reservations"
+    before_action :set_reservation, only: :destroy
+
     def index
-      user = User.find(params[:user_id])
-      reservations = user.reservations
-      cruise = []
-      reservations.each do |reserve|
-        cruise = Cruise.find(reserve.cruise_id)
-        cruise.push cruise
-      end
-      render json: cruise, adapter: :json_api, status: :ok
+      @reservations = current_user.reservations
     end
   
-    api :POST, '/users/:user_id/reservations', 'Create a new reservation for a cruise'
     def create
-      reservation = Reservation.create(user_id: params[:user_id], cruise_id: params[:cruise_id])
-      render json: reservation, adapter: :json_api, status: :ok if reservation.save!
+      @reservation = current_user.reservations.new(reservation_params)
+  
+      if @reservation.save
+        render :create, status: :created
+      else
+        render json: @reservation.errors, status: :unprocessable_entity
+      end
     end
   
-    api :DELETE, '/users/:user_id/reservations/:id', 'Delete a reservation for a cruise'
     def destroy
-      reservation = Reservation.find(params[:id])
-      reservation.destroy
-      render json: { message: 'Reservation has been deleted succesfully!', reservation: reservation.cruise.name },
-             adapter: :json_api, status: :ok
+      if @reservation.destroy
+        render json: { message: 'Reservation has been successfully deleted' }
+      else
+        render json: @reservation.errors, status: :unprocessable_entity
+      end
+    end
+  
+    private
+  
+    def set_reservation
+      @reservation = Reservation.find(params[:id])
+    end
+  
+    def reservation_params
+      params.require(:reservation).permit(:cruise :destination, :user_id)
     end
   end
