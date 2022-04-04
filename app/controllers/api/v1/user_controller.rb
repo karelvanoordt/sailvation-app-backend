0d96c866245e91630fe1e8fddc64f6fc1a8ec0c7
Karel van Oordt <vanoordtkarel@gmail.com>
2022-04-04 09:09:06 -0500

    Fix rubocop linters

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -1,12 +1,11 @@
 class UsersController < ApplicationController
-   
-    def index
-        @users = User.all
+  def index
+    @users = User.all
 
-        render json: @users
-    end
+    render json: @users
+  end
 
-    def destroy
-        @user.destroy        
-    end
+  def destroy
+    @user.destroy
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-    has_many :cruises
-    has_many :reservations
+  has_many :cruises
+  has_many :reservations
 end
