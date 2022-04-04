abe2459467c89717fb2a3b412247f7956f69927b
hamid-murambiwa <machipisajunior@gmail.com>
2022-04-04 15:46:15 +0200

    Update destination relationships and add data to database

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,5 +1,6 @@
 class Destination < ApplicationRecord
-    belongs_to :reservation
+    has_many :reservations
+
     validates :city, presence: true
     validates :country, presence: true
     validates :image, presence: true
