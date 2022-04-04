9ec2ef996489b5a6edc41b45f1db1bde3a84853f
hamid-murambiwa <machipisajunior@gmail.com>
2022-04-04 16:42:44 +0200

    Fix linters errors

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,7 +1,7 @@
 class Destination < ApplicationRecord
-    belongs_to :reservation
+  belongs_to :reservation
 
-    validates :city, presence: true
-    validates :country, presence: true
-    validates :image, presence: true
+  validates :city, presence: true
+  validates :country, presence: true
+  validates :image, presence: true
 end
