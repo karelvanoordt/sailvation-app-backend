a7bd421ea5fcc362b44ff3371967fe44be13848d
Karel van Oordt <vanoordtkarel@gmail.com>
2022-04-05 03:59:56 -0500

    Update detination index action

--- app/controllers/api/V1/destinations_controller.rb
+++ app/controllers/api/V1/destinations_controller.rb
@@ -1,6 +1,10 @@
 module Api
   module V1
     class DestinationsController < ApplicationController
+      def index
+        @destinations = Destination.all
+      end
+
       def show
         @destination = Destination.find(params[:id])
         render json: @destination
--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  namespace :api do
-    namespace :v1 do
-      resources :destinations, only: %i[create, show]
-    end
-  end
 
   # Defines the root path route ("/")
   # root "articles#index"
   namespace :api do
     namespace :v1 do
+      resources :destinations, only: %i[index show]
       resources :users do
         resources :cruises, only: %i[index show create destroy]
         resources :reservations, only: %i[index show create destroy]
