fe58faee706d0d6558e9d9cd1bec69f10d32f98a
Karel van Oordt <vanoordtkarel@gmail.com>
2022-04-05 05:47:21 -0500

    Update root page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
 
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "cruises#index"
+
   namespace :api do
     namespace :v1 do
       resources :destinations, only: %i[index show]
