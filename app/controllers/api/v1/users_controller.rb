7e147751c9385f48433dcf9c763fb354fb519e04
Karel van Oordt <vanoordtkarel@gmail.com>
2022-04-07 03:50:55 -0500

    Add user create and show methods

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,12 +3,28 @@ module Api
     class UsersController < ApplicationController
       def index
         @users = User.all
-
         render json: @users
+      end
+
+      def create
+        @user = User.new(user_params)
+        return render json: @user.errors, status: :unprocessable_entity unless @user.save
+        render json: @user, status: :created
+      end
+
+      def show
+        @reservations = Reservation.where(user_id: params[:id])
+        render json: @reservations
       end
 
       def destroy
         @user.destroy
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:name, :email)
       end
     end
   end
