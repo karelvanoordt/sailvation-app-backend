e7b2acf0d9179efa7e9c420686d110a1209fc65f
Karel van Oordt <vanoordtkarel@gmail.com>
2022-04-07 03:58:31 -0500

    Fix rubocop linters

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,6 +9,7 @@ module Api
       def create
         @user = User.new(user_params)
         return render json: @user.errors, status: :unprocessable_entity unless @user.save
+
         render json: @user, status: :created
       end
 
