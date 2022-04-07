642d8c0b763827030930f4d8585bd1acf6ea7b8c
Karel van Oordt <vanoordtkarel@gmail.com>
2022-04-07 03:47:22 -0500

    Add Email column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_06_084613) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_07_084549) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_06_084613) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
   add_foreign_key "cruises", "users"
